2e0abc619a4cf977edc482a9f5463cd732178399
Nicolas Poste <nicolas.poste@synaptix-labs.com>
2015-12-14 11:26:06 +0100

    message status: step

--- message-manager-shared/src/main/java/com/synaptix/mm/shared/model/domain/MessageStatus.java
+++ message-manager-shared/src/main/java/com/synaptix/mm/shared/model/domain/MessageStatus.java
@@ -8,35 +8,44 @@ public enum MessageStatus {
 	/**
 	 * The message has to be integrated (either it's its first time, either it's asked from an user who asked the message to be recycled).
 	 */
-	TO_BE_INTEGRATED,
+	TO_BE_INTEGRATED(0),
 	/**
 	 * The message has to be sent (either it's its first time, either it's asked from an user who asked the message to be recycled)
 	 */
-	TO_SEND,
+	TO_SEND(0),
 	/**
 	 * The message is currently in progress.
 	 * Transient status
 	 */
-	IN_PROGRESS,
+	IN_PROGRESS(1),
 	/**
 	 * The message can be recycled manually
 	 */
-	TO_RECYCLE_MANUALLY,
+	TO_RECYCLE_MANUALLY(3),
 	/**
 	 * The message will be recycled automatically according to the next processing date
 	 */
-	TO_RECYCLE_AUTOMATICALLY,
+	TO_RECYCLE_AUTOMATICALLY(3),
 	/**
 	 * The message has successfully been integrated
 	 */
-	INTEGRATED,
+	INTEGRATED(2),
 	/**
 	 * The message has successfully been sent
 	 */
-	SENT,
+	SENT(2),
 	/**
 	 * The message has definitely been rejected
 	 */
-	REJECTED
+	REJECTED(4);
 
+	private final int step;
+
+	private MessageStatus(int step) {
+		this.step = step;
+	}
+
+	public int getStep() {
+		return step;
+	}
 }
