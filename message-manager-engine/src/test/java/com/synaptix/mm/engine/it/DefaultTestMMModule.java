302277702de462c9c21310805f6d64753c890c6a
Nicolas Poste <nicolas.poste@talanlabs.com>
2016-04-01 14:07:18 +0200

    tests for better coverage

--- message-manager-engine/src/test/java/com/synaptix/mm/engine/it/DefaultTestMMModule.java
+++ message-manager-engine/src/test/java/com/synaptix/mm/engine/it/DefaultTestMMModule.java
@@ -3,6 +3,7 @@ package com.synaptix.mm.engine.it;
 import com.google.inject.AbstractModule;
 import com.google.inject.Singleton;
 import com.synaptix.mm.engine.factory.IProcessErrorFactory;
+import com.synaptix.mm.shared.model.IIntegConfig;
 
 /**
  * Created by NicolasP on 22/10/2015.
@@ -18,6 +19,8 @@ public class DefaultTestMMModule extends AbstractModule {
 	@Override
 	protected final void configure() {
 		bind(IProcessErrorFactory.class).to(processErrorFactoryClass).in(Singleton.class);
+
+		bind(IIntegConfig.class).toInstance(new DefaultIntegConfig());
 
 		configureTestModule();
 	}
--- message-manager-server/src/test/java/com/synaptix/mm/server/it/ITMessageContent.java
+++ message-manager-server/src/test/java/com/synaptix/mm/server/it/ITMessageContent.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 
 import org.apache.commons.vfs2.FileObject;
-import org.apache.commons.vfs2.FileSystemException;
 import org.apache.commons.vfs2.FileSystemManager;
 import org.apache.commons.vfs2.Selectors;
 import org.apache.commons.vfs2.VFS;
@@ -28,126 +27,113 @@ public class ITMessageContent extends AbstractMMTest {
 	@Inject
 	private FluxContentServiceDelegate fluxContentServiceDelegate;
 
-	/**
-	 * The test needs to be played from the project root, that's why there is a main here
-	 */
-	public static void main(String[] args) {
-		ITMessageContent mainFluxContent = new ITMessageContent();
+	@Test
+	public void testNew() throws Exception {
+		IFSMessage message = new DefaultMessage("test");
 
-		mainFluxContent.init();
+		message.setFolder(".." + File.separator + "messageTest");
 
+		boolean exceptionRaised = false;
 		try {
-			mainFluxContent.testNew();
-		} catch (Throwable t) {
-			t.printStackTrace();
+			System.out.println("Testing get content for 1st case");
+			Assert.assertNull(fluxContentServiceDelegate.getContent(message));
+		} catch (ContentNotFetchedException e) {
+			exceptionRaised = e.getCause() instanceof FileNotFoundException;
 		}
-		try {
-			mainFluxContent.testArchive();
-		} catch (Throwable t) {
-			t.printStackTrace();
-		}
+		Assert.assertTrue(exceptionRaised);
 
-		mainFluxContent.waitIntegrator();
+		System.out.println("Testing set content for 1st case");
+		fluxContentServiceDelegate.setContent("test", message);
 
-		mainFluxContent.stopIntegrator();
+		System.out.println("Testing get content after set content for 1st case");
+		Assert.assertEquals("test", fluxContentServiceDelegate.getContent(message));
+
+		System.out.println("Testing set content 2 for 1st case");
+		fluxContentServiceDelegate.setContent("test plus complet", message);
+
+		System.out.println("Testing get content after set content 2 for 1st case");
+		Assert.assertEquals("test plus complet", fluxContentServiceDelegate.getContent(message));
 	}
 
 	@Test
-	public void testNew() {
-		try {
-			IFSMessage message = new DefaultMessage("test");
-
-			message.setFolder(".." + File.separator + "messageTest");
-
-			boolean exceptionRaised = false;
-			try {
-				System.out.println("Testing get content for 1st case");
-				Assert.assertNull(fluxContentServiceDelegate.getContent(message));
-			} catch (ContentNotFetchedException e) {
-				exceptionRaised = e.getCause() instanceof FileNotFoundException;
-			}
-			Assert.assertTrue(exceptionRaised);
-
-			System.out.println("Testing set content for 1st case");
-			fluxContentServiceDelegate.setContent("test", message);
-
-			System.out.println("Testing get content after set content for 1st case");
-			Assert.assertEquals("test", fluxContentServiceDelegate.getContent(message));
-
-			System.out.println("Testing set content 2 for 1st case");
-			fluxContentServiceDelegate.setContent("test plus complet", message);
-
-			System.out.println("Testing get content after set content 2 for 1st case");
-			Assert.assertEquals("test plus complet", fluxContentServiceDelegate.getContent(message));
-
-		} catch (Exception e) {
-			e.printStackTrace();
-			Assert.fail();
-		}
-	}
-
-	@Test
-	public void testArchive() {
+	public void testArchive() throws Exception {
 
 		File file = new File("./src/test/flux/contentTest.tgz");
 		Assert.assertTrue(file.exists());
 
+		FileSystemManager manager = VFS.getManager();
+		FileObject fileObject = manager.resolveFile(file.getAbsolutePath());
+		FileObject destObject = manager.resolveFile("./contentTest.tgz");
+		destObject.copyFrom(fileObject, Selectors.SELECT_SELF);
+
+		IFSMessage message = new DefaultMessage("test", "87e8ded7-b3a9-486c-a947-95bd1e6e61d0");
+
+		message.setFolder(".." + File.separator + "contentTest");
+
+		System.out.println("Testing get content for 2nd case");
+		Assert.assertEquals("OK", fluxContentServiceDelegate.getContent(message));
+
+		boolean exceptionRaised = false;
 		try {
-			FileSystemManager manager = VFS.getManager();
-			FileObject fileObject = manager.resolveFile(file.getAbsolutePath());
-			FileObject destObject = manager.resolveFile("./contentTest.tgz");
-			destObject.copyFrom(fileObject, Selectors.SELECT_SELF);
-		} catch (FileSystemException e) {
-			e.printStackTrace();
-			Assert.fail(e.getMessage());
+			System.out.println("Testing set content for 2nd case");
+			fluxContentServiceDelegate.setContent("test", message);
+		} catch (ContentNotSavedException e) {
+			exceptionRaised = true;
 		}
-
-		try {
-			IFSMessage message =  new DefaultMessage("test", "87e8ded7-b3a9-486c-a947-95bd1e6e61d0");
-
-			message.setFolder(".." + File.separator + "contentTest");
-
-			System.out.println("Testing get content for 2nd case");
-			Assert.assertEquals("OK", fluxContentServiceDelegate.getContent(message));
-
-			boolean exceptionRaised = false;
-			try {
-				System.out.println("Testing set content for 2nd case");
-				fluxContentServiceDelegate.setContent("test", message);
-			} catch (ContentNotSavedException e) {
-				exceptionRaised = true;
-			}
-			Assert.assertTrue(exceptionRaised);
-
-		} catch (Exception e) {
-			e.printStackTrace();
-			Assert.fail(e.getMessage());
-		}
+		Assert.assertTrue(exceptionRaised);
 	}
 
 	@Test
-	public void testFlux() {
+	public void testFlux() throws Exception {
+		IFSMessage message = new DefaultMessage(null);
+		IId id = new IdRaw(message.getId().toString());
+
+		Assert.assertNull(fluxContentServiceDelegate.getTestFluxContent(id));
+
+		String text = "test";
+		fluxContentServiceDelegate.setTestFluxContent(text, id);
+		Assert.assertEquals(text, fluxContentServiceDelegate.getTestFluxContent(id));
+
+		text = "test2";
+		fluxContentServiceDelegate.setTestFluxContent(text, id);
+		Assert.assertEquals(text, fluxContentServiceDelegate.getTestFluxContent(id));
+
+		text = null;
+		fluxContentServiceDelegate.setTestFluxContent(text, id);
+		Assert.assertNull(fluxContentServiceDelegate.getTestFluxContent(id));
+	}
+
+	@Test
+	public void exceptionMessage() throws Exception {
+		IFSMessage message = new DefaultMessage(null);
+		IId id = new IdRaw(message.getId().toString());
+
+		boolean exceptionRaised = false;
 		try {
-			IFSMessage message = new DefaultMessage(null);
-
-			IId id = new IdRaw(message.getId().toString());
-			Assert.assertNull(fluxContentServiceDelegate.getTestFluxContent(id));
-
-			String text = "test";
-			fluxContentServiceDelegate.setTestFluxContent(text, id);
-			Assert.assertEquals(text, fluxContentServiceDelegate.getTestFluxContent(id));
-
-			text = "test2";
-			fluxContentServiceDelegate.setTestFluxContent(text, id);
-			Assert.assertEquals(text, fluxContentServiceDelegate.getTestFluxContent(id));
-
-			text = null;
-			fluxContentServiceDelegate.setTestFluxContent(text, id);
-			Assert.assertNull(fluxContentServiceDelegate.getTestFluxContent(id));
-
-		} catch (Exception e) {
-			e.printStackTrace();
-			Assert.fail();
+			fluxContentServiceDelegate.setContent("test", null);
+		} catch (ContentNotSavedException e) {
+			exceptionRaised = true;
 		}
+		Assert.assertTrue(exceptionRaised);
+
+		exceptionRaised = false;
+		try {
+			fluxContentServiceDelegate.setContent("test", message);
+		} catch (ContentNotSavedException e) {
+			exceptionRaised = true;
+		}
+		Assert.assertTrue(exceptionRaised);
+
+		message.setFolder("contentTest");
+
+		message = new DefaultMessage("");
+		exceptionRaised = false;
+		try {
+			fluxContentServiceDelegate.setContent("test", message);
+		} catch (ContentNotSavedException e) {
+			exceptionRaised = true;
+			e.printStackTrace();
+		}
+		Assert.assertTrue(exceptionRaised);
 	}
 }
