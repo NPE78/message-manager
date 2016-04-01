cf1368e094a27028ead6fe8ad9814b312ef2bd3b
Nicolas Poste <nicolas.poste@talanlabs.com>
2016-04-01 16:14:19 +0200

    test archive

--- message-manager-server/src/test/java/com/synaptix/mm/server/it/ITArchive.java
+++ message-manager-server/src/test/java/com/synaptix/mm/server/it/ITArchive.java
@@ -1,6 +1,5 @@
 package com.synaptix.mm.server.it;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import com.synaptix.mm.server.agent.BatchArchiveAgent;
@@ -8,7 +7,6 @@ import com.synaptix.mm.server.agent.BatchArchiveAgent;
 /**
  * Created by NicolasP on 31/03/2016.
  */
-@Ignore
 public class ITArchive extends AbstractMMTest {
 
 	@Test
