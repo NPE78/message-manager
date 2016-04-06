172ac86ac777d328c4903d14fdaa9b2ea8b1916b
Nicolas Poste <nicolas.poste@talanlabs.com>
2016-04-06 10:37:15 +0200

    dictionary utils

---
+++ message-manager-supervision/src/main/java/com/synaptix/mm/supervision/utils/DictionaryUtils.java
@@ -0,0 +1,63 @@
+package com.synaptix.mm.supervision.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.google.inject.Inject;
+import com.synaptix.mm.engine.MMDictionary;
+import com.synaptix.mm.engine.SubDictionary;
+import com.synaptix.mm.engine.exception.UnknownDictionaryException;
+import com.synaptix.mm.shared.model.IErrorType;
+import com.synaptix.mm.supervision.model.DictionaryModelDto;
+
+/**
+ * Created by NicolasP on 06/04/2016.
+ */
+public class DictionaryUtils {
+
+	@Inject
+	private static MMDictionary dictionary;
+
+	private DictionaryUtils() {
+	}
+
+	public static DictionaryModelDto getDictionaryDto(String subDictionary) throws UnknownDictionaryException {
+		SubDictionary dico = dictionary;
+		if (StringUtils.isNotBlank(subDictionary)) {
+			dico = dictionary.getSubsetDictionary(subDictionary);
+		}
+
+		Map<String, IErrorType> errorMap = dico.getErrorMap();
+		DictionaryModelDto root = new DictionaryModelDto("MAIN"); //$NON-NLS-1$
+		Map<String, DictionaryModelDto> map = new HashMap<>();
+		for (Map.Entry<String, IErrorType> entry : errorMap.entrySet()) {
+			DictionaryModelDto currentModel = root;
+			String[] dics = entry.getKey().split("\\."); //$NON-NLS-1$
+			String r = "";
+			for (int i = 0; i < dics.length - 1; i++) {
+				String d = dics[i];
+				r = (r.isEmpty() ? "" : r + ".") + d;
+				DictionaryModelDto newModel = map.get(r);
+				if (newModel == null) {
+					newModel = new DictionaryModelDto(d);
+					currentModel.getSubDictionaryList().add(newModel);
+					map.put(r, newModel);
+				}
+				currentModel = newModel;
+			}
+
+			IErrorType value = entry.getValue();
+			String v;
+			if (value.getNextRecyclingDuration() != null) {
+				v = String.format("%s (%s, %d)", value.getCode(), value.getRecyclingKind(), value.getNextRecyclingDuration());
+			} else {
+				v = String.format("%s (%s)", value.getCode(), value.getRecyclingKind());
+			}
+			currentModel.getErrorList().add(v);
+		}
+
+		return root;
+	}
+}
---
+++ message-manager-supervision/src/test/java/com/synaptix/mm/supervision/utils/DictionaryUtilsTest.java
@@ -0,0 +1,41 @@
+package com.synaptix.mm.supervision.utils;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.google.inject.Inject;
+import com.synaptix.mm.engine.MMDictionary;
+import com.synaptix.mm.engine.SubDictionary;
+import com.synaptix.mm.engine.model.DefaultErrorType;
+import com.synaptix.mm.shared.model.domain.ErrorRecyclingKind;
+import com.synaptix.mm.supervision.model.DictionaryModelDto;
+
+/**
+ * Created by NicolasP on 29/10/2015.
+ */
+public class DictionaryUtilsTest extends AbstractSupervisionTest {
+
+	@Inject
+	private MMDictionary dictionary;
+
+	@Test
+	public void testDictionaryUtils() throws Exception {
+
+		dictionary.defineError(new DefaultErrorType("TEST", ErrorRecyclingKind.NOT_RECYCLABLE));
+
+		SubDictionary sub = this.dictionary.addSubsetDictionary("sub");
+
+		SubDictionary ter = sub.addSubsetDictionary("ter");
+		ter.defineError(new DefaultErrorType("TEST2", ErrorRecyclingKind.AUTOMATIC, 120));
+
+		DictionaryModelDto dictionaryDto = DictionaryUtils.getDictionaryDto(null);
+		Assert.assertEquals(1, dictionaryDto.getErrorList().size());
+		Assert.assertTrue(dictionaryDto.getErrorList().get(0).startsWith("TEST "));
+		Assert.assertEquals(1, dictionaryDto.getSubDictionaryList().size());
+		Assert.assertEquals("sub", dictionaryDto.getSubDictionaryList().get(0).getDictionaryName());
+		Assert.assertEquals(0, dictionaryDto.getSubDictionaryList().get(0).getErrorList().size());
+		Assert.assertEquals(1, dictionaryDto.getSubDictionaryList().get(0).getSubDictionaryList().size());
+		Assert.assertEquals(1, dictionaryDto.getSubDictionaryList().get(0).getSubDictionaryList().get(0).getErrorList().size());
+		Assert.assertTrue(dictionaryDto.getSubDictionaryList().get(0).getSubDictionaryList().get(0).getErrorList().get(0).startsWith("TEST2 "));
+	}
+}
