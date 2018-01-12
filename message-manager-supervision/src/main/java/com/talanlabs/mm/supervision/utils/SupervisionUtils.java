b418bf679131d55b0281cb6de5cd83f1eb0e943e
Nicolas Poste <nicolas.poste@talanlabs.com>
2018-01-12 16:39:52 +0100

    sonar

--- message-manager-supervision/src/main/java/com/talanlabs/mm/supervision/utils/SupervisionUtils.java
+++ message-manager-supervision/src/main/java/com/talanlabs/mm/supervision/utils/SupervisionUtils.java
@@ -19,36 +19,37 @@ public final class SupervisionUtils {
     private static Comparator<AgentInfoDto> COMPARATOR = (o1, o2) -> o1.getNbWorking().compareTo(o2.getNbWorking()) * (-1);
 
     private SupervisionUtils() {
-	}
+    }
 
-	/**
-	 * Get the current agent info of all agents used at the present time
-	 * @param agentName optional, use this to show only the selected agent info
-	 * @return the list of currently used agents with their running count, waiting count... ordered by running+waiting (descending)
-	 */
-	public static List<AgentInfoDto> getAgentInfo(String engineUuid, String agentName) {
-		List<AgentInfoDto> agentInfoDtos = new ArrayList<>();
+    /**
+     * Get the current agent info of all agents used at the present time
+     *
+     * @param agentName optional, use this to show only the selected agent info
+     * @return the list of currently used agents with their running count, waiting count... ordered by running+waiting (descending)
+     */
+    public static List<AgentInfoDto> getAgentInfo(String engineUuid, String agentName) {
+        List<AgentInfoDto> agentInfoDtos = new ArrayList<>();
         Engine engine = ProcessManager.getEngine(engineUuid);
         Collection<ChannelSlot> channels = engine.getChannelSlots();
-		channels.stream().filter(channelSlot -> (agentName == null) || (agentName.isEmpty()) || (agentName.equals(channelSlot.getName()))).forEach(channelSlot -> {
-			AgentInfoDto agentInfoDto = new AgentInfoDto();
-			agentInfoDto.setName(channelSlot.getName());
-			agentInfoDto.setAvailable(channelSlot.isAvailable());
-			agentInfoDto.setBusy(channelSlot.isBusy());
-			PluggableChannel pluggedChannel = channelSlot.getPluggedChannel();
-			if (pluggedChannel instanceof ProcessingChannel) {
-				ProcessingChannel processingChannel = (ProcessingChannel) pluggedChannel;
-				agentInfoDto.setOverloaded(processingChannel.isOverloaded());
-				agentInfoDto.setMeaning(pluggedChannel.toString());
-				agentInfoDto.setNbWorking(pluggedChannel.getNbWorking());
-			}
-			agentInfoDtos.add(agentInfoDto);
-		});
-		sortAgentInfo(agentInfoDtos);
-		return agentInfoDtos;
-	}
+        channels.stream().filter(channelSlot -> (agentName == null) || (agentName.isEmpty()) || (agentName.equals(channelSlot.getName()))).forEach(channelSlot -> {
+            AgentInfoDto agentInfoDto = new AgentInfoDto();
+            agentInfoDto.setName(channelSlot.getName());
+            agentInfoDto.setAvailable(channelSlot.isAvailable());
+            agentInfoDto.setBusy(channelSlot.isBusy());
+            PluggableChannel pluggedChannel = channelSlot.getPluggedChannel();
+            if (pluggedChannel instanceof ProcessingChannel) {
+                ProcessingChannel processingChannel = (ProcessingChannel) pluggedChannel;
+                agentInfoDto.setOverloaded(processingChannel.isOverloaded());
+                agentInfoDto.setMeaning(pluggedChannel.toString());
+                agentInfoDto.setNbWorking(pluggedChannel.getNbWorking());
+            }
+            agentInfoDtos.add(agentInfoDto);
+        });
+        sortAgentInfo(agentInfoDtos);
+        return agentInfoDtos;
+    }
 
-	private static void sortAgentInfo(List<AgentInfoDto> agentInfoDtos) {
-		agentInfoDtos.sort(COMPARATOR);
-	}
+    private static void sortAgentInfo(List<AgentInfoDto> agentInfoDtos) {
+        agentInfoDtos.sort(COMPARATOR);
+    }
 }
